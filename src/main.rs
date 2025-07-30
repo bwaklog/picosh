2e645db5c5e2651ab3e1215ef48dc7f84a257fcc
Aditya Hegde <burrawta@gmail.com>
2025-07-30 16:16:05 +0530

    chore: cli documentation and cleanup

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,11 @@ use serialport::SerialPort;
 
 const LOAD_PROG_MAGIC: &[u8] = "LOADPROG".as_bytes();
 const KILL_PROG_MAGIC: &[u8] = "KILLTASK".as_bytes();
-const LIST_TASKS_MAGIC: &[u8] = "LISTTASK".as_bytes();
+const LIST_TASKS_MAGIC: &[u8] = "LISTPROG".as_bytes();
 const RELOAD_TASKS_MAGIC: &[u8] = "RELAUNCH".as_bytes();
 
+/// Serial Interface for the PicOS project
+/// (gh: https://github.com/pes-innovation-lab/possum)
 #[derive(Parser, Debug)]
 struct Args {
     /// The directives for the program
@@ -23,7 +25,7 @@ struct Args {
     cmd: Commands,
 
     /// The /dev device for the Pico
-    #[arg(long)]
+    #[arg(short, long)]
     device: String,
     /// Baudrate for UART
     #[arg(short, long, default_value_t = 115200)]
@@ -32,6 +34,7 @@ struct Args {
 
 #[derive(Subcommand, Debug, Clone)]
 enum Commands {
+    /// Load a program built with PIC to the Pico
     Load {
         file: String,
         /// symbol for the entry point of the program
@@ -39,15 +42,19 @@ enum Commands {
         /// 8 byte identifier
         identifier: String,
     },
+    /// Kill a task on the pico
     Kill {
         /// The id of the taks to be killed
         identifier: String,
     },
+    /// Relaunch a task on the Pico
     Relaunch {
         /// The id of the taks to be killed
         identifier: String,
     },
+    /// List all tasks and status on the Pico
     List,
+    /// Serial logger
     Log,
 }
 
@@ -68,7 +75,7 @@ fn handle_load_cmd(file: String, symbol: String, identifier: String) -> Option<V
 
     let metadata = std::fs::metadata(path.clone()).unwrap();
     let size = metadata.size().to_le_bytes();
-    println!("program size {} {:?}", metadata.size(), size);
+    println!("[PICOSH] program size {} {:?}", metadata.size(), size);
     data.extend(&size[0..8]);
 
     let symbol_table = file.symbol_table().unwrap().unwrap();
@@ -78,24 +85,26 @@ fn handle_load_cmd(file: String, symbol: String, identifier: String) -> Option<V
         .0
         .into_iter()
         .find(|s| symbol.clone() == string_talbe.get(s.st_name as usize).unwrap())
-        .expect(&format!("symbol with name {} not found", symbol.clone()));
+        .unwrap_or_else(|| panic!("symbol with name {} not found", symbol.clone()));
 
     let symbol_address = symbols.st_value;
     let symbol_address_bytes = &symbol_address.to_le_bytes()[0..8];
-    println!(
-        "symbol address 0X{:X} => {:?}",
-        symbol_address, symbol_address_bytes
-    );
+    println!("[PICOSH] symbol address 0X{symbol_address:X} => {symbol_address_bytes:?}");
 
-    data.extend_from_slice(&symbol_address_bytes);
+    data.extend_from_slice(symbol_address_bytes);
 
-    let inter_trim: String = format!("{:8}", identifier).chars().take(8).collect();
+    let inter_trim: String = format!("{identifier:8}").chars().take(8).collect();
     let inter_bytes = inter_trim.clone().into_bytes();
     data.extend(&inter_bytes);
-    println!("{} => {:?}, {}", inter_trim, inter_bytes, inter_bytes.len());
+    println!(
+        "[PICOSH] {} => {:?}, {}",
+        inter_trim,
+        inter_bytes,
+        inter_bytes.len()
+    );
 
     data.extend(file_data.as_slice());
-    println!("writing {} bytes do data", metadata.size());
+    println!("[PICOSH] writing {} bytes do data", metadata.size());
     // data.extend(file_data.as_slice());
 
     // buf_writer.flush().expect("failed to flush file");
@@ -106,14 +115,14 @@ fn handle_load_cmd(file: String, symbol: String, identifier: String) -> Option<V
 fn handle_kill_cmd(identifier: String) -> Option<Vec<u8>> {
     let mut data: Vec<u8> = Vec::new();
 
-    let ident_bytes = format!("{:8}", identifier)
+    let ident_bytes = format!("{identifier:8}")
         .chars()
         .take(8)
         .collect::<String>()
         .into_bytes();
 
     println!(
-        "Attempting to kill program {:?} => {}",
+        "[PICOSH] Attempting to kill program {:?} => {}",
         ident_bytes.clone(),
         String::from_utf8(ident_bytes.clone()).unwrap()
     );
@@ -127,14 +136,14 @@ fn handle_kill_cmd(identifier: String) -> Option<Vec<u8>> {
 fn handle_relaunch_cmd(identifier: String) -> Option<Vec<u8>> {
     let mut data: Vec<u8> = Vec::new();
 
-    let ident_bytes = format!("{:8}", identifier)
+    let ident_bytes = format!("{identifier:8}")
         .chars()
         .take(8)
         .collect::<String>()
         .into_bytes();
 
     println!(
-        "Attempting to kill program {:?} => {}",
+        "[PICOSH] Attempting to kill program {:?} => {}",
         ident_bytes.clone(),
         String::from_utf8(ident_bytes.clone()).unwrap()
     );
@@ -153,6 +162,7 @@ fn handle_list_cmd() -> Option<Vec<u8>> {
     Some(data)
 }
 
+// #[allow(unused_assignments)]
 fn handle_command(
     cmd: Commands,
     serial: Arc<Mutex<Box<dyn SerialPort + 'static>>>,
@@ -177,22 +187,29 @@ fn handle_command(
 
         Commands::Relaunch { identifier } => result = handle_relaunch_cmd(identifier)?,
         Commands::Kill { identifier } => result = handle_kill_cmd(identifier)?,
-        Commands::Log => {}
+        Commands::Log => {
+            drop(result);
+            return None;
+        }
     }
 
-    println!("{:?}", result);
+    println!(
+        "[PICOSH] writing {} bytes to dump file /tmp/elf.dump and serial device",
+        result.len()
+    );
+
     dump_file
         .write_all(&result)
         .expect("failed to write data to the dumpfile");
 
     let mut writer_handle = serial.lock().unwrap();
     for byte in result {
-        writer_handle.write(&[byte]).unwrap();
+        writer_handle.write_all(&[byte]).unwrap();
         writer_handle.flush().unwrap();
     }
 
     writer_handle.flush().unwrap();
-    return None;
+    None
 }
 
 fn main() {
@@ -201,7 +218,7 @@ fn main() {
     let serial = Arc::new(Mutex::new(
         serialport::new(args.device.clone(), args.baudrate)
             .open()
-            .expect(&format!("unable to open device {}", args.device.clone())),
+            .unwrap_or_else(|_| panic!("unable to open device {}", args.device.clone())),
     ));
 
     let reader_serial = Arc::clone(&serial);
@@ -211,9 +228,8 @@ fn main() {
         loop {
             let handler_result = reader_serial.lock();
 
-            if !handler_result.is_err() {
+            if let Ok(mut handler) = handler_result {
                 let mut read_buf: Vec<u8> = vec![0; 1];
-                let mut handler = handler_result.unwrap();
                 _ = handler.read_exact(read_buf.as_mut_slice());
                 print!("{}", String::from_utf8_lossy(&read_buf));
                 stdout().flush().unwrap();
@@ -222,7 +238,9 @@ fn main() {
     });
 
     sleep(time::Duration::from_secs(2));
-    handle_command(args.cmd, Arc::clone(&serial));
+    _ = handle_command(args.cmd, Arc::clone(&serial));
 
-    loop {}
+    loop {
+        thread::sleep(time::Duration::from_secs(1));
+    }
 }
